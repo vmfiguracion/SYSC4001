febab35f5cf2cb261b84215b54a614fd325f7f08
Valerie Figuracion <47678410+vmfiguracion@users.noreply.github.com>
2020-02-14 13:09:26 -0500

    Fixed the logic of MatrixMultiplication.c

--- SYSC4001/Assignment1/shm_com.h
+++ SYSC4001/Assignment1/shm_com.h
@@ -1,8 +1,5 @@
 /* A common header file to describe the shared memory we wish to pass about. */
 
-#define TEXT_SZ 2048
-
 struct shared_use_st {
 	int shared_matrix[4][4];
-    char some_text[TEXT_SZ];
 };
